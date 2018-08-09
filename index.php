0791ecaa0848fd48a8561633214da6bb8b461010
Somberlord <somberlord@gmail.com>
2018-08-09 17:37:06 +0200

    Change DO ID to TSV Number in page display

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $allusers = pg_fetch_all($queryres);
 
 if( isset ($_POST["userid"]) ) {
   $userid = $_POST["userid"];
-  $tsvres = pg_query_params($conn, "select tu.name as username, tt.game_name as user_gamename, tt.doid as doid, tp.pokemon as pokemon, tp.game_name as gamename, tp.save_nb as save_nb, tp.box_nb as box_nb, tp.row as row, tp.line as line, tp.id_tsv as fournee, tt.gen as gen ".
+  $tsvres = pg_query_params($conn, "select tu.name as username, tt.game_name as user_gamename, tt.tsvnumber as tsvnumber, tp.pokemon as pokemon, tp.game_name as gamename, tp.save_nb as save_nb, tp.box_nb as box_nb, tp.row as row, tp.line as line, tp.id_tsv as fournee, tt.gen as gen ".
                       "from tsv_user tu ".
                       "left join tsv_tsvnumber tt on tt.uuid = tu.uid ".
                       "left join tsv_pokemon tp on tp.esv = tt.tsvnumber and tp.gen = tt.gen ".
@@ -35,7 +35,7 @@ if( isset ($_POST["userid"]) ) {
     <tr>
       <td>Utilisateur</td>
       <td>Jeu utilisateur</td>
-      <td>ID utilisateur</td>
+      <td>TSV Utilisateur</td>
       <td>Nom du pokemon</td>
       <td>Jeu de generation</td>
       <td>Numero de save</td>
@@ -47,7 +47,7 @@ if( isset ($_POST["userid"]) ) {
       <tr>
         <td><? echo $value['username'] ?></td>
         <td><? echo $value['user_gamename'] ?></td>
-        <td><? echo $value['doid'] ?></td>
+        <td><? echo $value['tsvnumber'] ?></td>
         <td><? echo $value['pokemon'] ?></td>
         <td><? echo $value['gamename'] ?></td>
         <td><? echo $value['save_nb'] ?></td>
